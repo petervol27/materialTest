658c3759ea56d9c7ee23fa8d27f4c1b706352280
Pietro <petervol35@gmail.com>
2025-01-31 11:18:09 +0200

    date picker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,6 +1,7 @@
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+
 export default function ResponsiveDatePickers() {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
